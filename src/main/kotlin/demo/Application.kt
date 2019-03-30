8d4458a2a434c89e365a8ca205e1d0a92e99d261
Antonio Muñoz <antoniogmc@gmail.com>
2019-03-30 12:25:10 +0100

    updated to spring-boot 2.1.3

--- src/main/kotlin/demo/Application.kt
+++ src/main/kotlin/demo/Application.kt
@@ -1,11 +1,11 @@
 package demo
 
-import org.springframework.boot.SpringApplication
+import org.springframework.boot.runApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
 class Application
 
 fun main(args: Array<String>) {
-  SpringApplication.run(Application::class.java, *args)
+  runApplication<Application>(*args)
 }
--- src/test/kotlin/demo/GreeterTest.kt
+++ src/test/kotlin/demo/GreeterTest.kt
@@ -1,11 +1,17 @@
 package demo
 
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.junit4.SpringRunner
 import kotlin.test.assertEquals
 
+@RunWith(SpringRunner::class)
 class GreeterTest {
+  
+  @Autowired lateinit var greeter: Greeter
+  
   @Test fun greetingsTest() {
-    val greeter = Greeter()
     assertEquals("Hello World!", greeter.greetings("World"))
   }
 }
